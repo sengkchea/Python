4dce2a044cc3fc8b84c6d816d483b753890f8e85
Seng Chea <sengkchea@gmail.com>
2022-04-18 21:46:35 -0700

    Added Player class and game setup logic

--- Milestone Project #2/War Card Game.py
+++ Milestone Project #2/War Card Game.py
@@ -1,3 +1,4 @@
+from hashlib import new
 import random
 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
@@ -36,17 +37,85 @@ class Deck:
     def deal_one(self):
         return self.all_cards.pop()
 
-two_hearts = Card("Hearts","Two")
-print(two_hearts)
-print(two_hearts.suit)
-print(two_hearts.rank)
-print(values[two_hearts.rank])
+#Player class
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
+
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self,new_cards):
+        #check if adding multiple cards or just a single card
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+#Game setup
+player_one = Player("One")
+player_two = Player("Two")
+
 new_deck = Deck()
-first_card = new_deck.all_cards[0]
-print(first_card.suit)
 new_deck.shuffle()
-for card_object in new_deck.all_cards:
-    print(card_object)
-new_card = new_deck.deal_one()
-print(new_card)
-print(len(new_deck.all_cards)) 
+
+#deal cards for both players
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+round_num = 0
+
+while game_on:
+    round_num += 1
+    print(f"Round {round_num}")
+
+    if len(player_one.all_cards) == 0:
+        print(f'Player {player_one.name} is out of cards. Player {player_two.name} wins!')
+        game_on = False
+        break
+
+    if len(player_two.all_cards) == 0:
+        print(f'Player {player_two.name} is out of cards. Player {player_one.name} wins!')
+        game_on = False
+        break
+    
+    #Start new round
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+
+    #while at_war     
+    
+
+
+# two_hearts = Card("Hearts","Two")
+# print(two_hearts)
+# print(two_hearts.suit)
+# print(two_hearts.rank)
+# print(values[two_hearts.rank])
+# new_deck = Deck()
+# first_card = new_deck.all_cards[0]
+# print(first_card.suit)
+# new_deck.shuffle()
+# for card_object in new_deck.all_cards:
+#     print(card_object)
+# new_card = new_deck.deal_one()
+# print(new_card)
+# print(len(new_deck.all_cards)) 
+# new_player = Player("Seng")
+# print(new_player)
+# new_player.add_cards(first_card)
+# print(new_player)
+# new_player.add_cards([first_card, first_card, first_card])
+# print(new_player)
+# new_player.remove_one()
+# print(new_player)
